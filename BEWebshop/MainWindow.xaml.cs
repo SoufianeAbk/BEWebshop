be4b06ad9f697d7735a44606c00ffa18d77d72c9
SoufianeAbk <229384282+sso-vbk@users.noreply.github.com>
2025-10-28 15:58:14 +0100

    Quick Update.

--- BEWebshop/MainWindow.xaml.cs
+++ BEWebshop/MainWindow.xaml.cs
@@ -1,24 +1,35 @@
-﻿using System.Text;
+﻿using System.Globalization;
 using System.Windows;
-using System.Windows.Controls;
 using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
 using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
+using BEWebshop.ViewModels;
 
 namespace BEWebshop
 {
-    /// <summary>
-    /// Interaction logic for MainWindow.xaml
-    /// </summary>
     public partial class MainWindow : Window
     {
         public MainWindow()
         {
             InitializeComponent();
+            DataContext = new MainViewModel();
+        }
+    }
+
+    // Simple converter for button highlighting in navigation
+    public class ViewToColorConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            if (value?.ToString() == parameter?.ToString())
+            {
+                return new SolidColorBrush(Color.FromRgb(0, 90, 158)); // Darker blue for active
+            }
+            return new SolidColorBrush(Color.FromRgb(0, 122, 204)); // Regular blue
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
         }
     }
 }
